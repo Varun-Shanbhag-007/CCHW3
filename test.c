e353922ef41636c4680823a76ee81702e678b10e
Chameleon Cloud User <cc@ubuntu-18-team11.novalocal>
2020-03-28 08:04:54 +0000

    added code for sequential read

--- test.c
+++ test.c
@@ -9,7 +9,7 @@
 using namespace std;
 using namespace std::chrono;
 
-#define BLOCKSIZE 16*1024*1024
+#define BLOCKSIZE 1024*1024
 
 int main (void)
 {
@@ -17,16 +17,6 @@ int main (void)
 
     
     long long int record_size = 1024*1024;
-
-    char *buf1 = (char *)malloc(1024*1024*sizeof(char));
-    long long int i =0;
-    
-    for(i =0;i<record_size;i++){
-
-            buf1[i] = 'a';
-    }
-
-    buf1[i] = '\0';
 
     void *buffer;
     
@@ -38,16 +28,14 @@ int main (void)
     
     posix_memalign(&buffer, BLOCKSIZE, BLOCKSIZE);
 	
-    memcpy(buffer, buf1, 1024*1024*sizeof(char));
-
     cout<<buffer<<endl<<sizeof(buffer)<<endl;
 
-    int a = open("f1.txt", O_CREAT|O_TRUNC|O_DIRECT|O_WRONLY, S_IRWXU);
+    int a = open("0", O_DIRECT|O_RDONLY, S_IRWXU);
 
     auto start = high_resolution_clock::now();
-    for(int i =0;i<10000;i++){
+    for(int i =0;i<10240;i++){
 
-    int error = write(a, buffer, BLOCKSIZE);
+    int error = read(a, buffer, BLOCKSIZE);
     //  cout<<error;
     }
 
@@ -56,11 +44,9 @@ int main (void)
 
     close(a);
     free(buffer);
-    free(buf1);
 
     auto duration = duration_cast<seconds>(stop - start);
-     cout << "--Time taken by function: "
-    <<(size/1000)/duration.count() << " kB/seconds" << endl;
+     cout << "--Time taken by function: "<<duration.count();
 
     return 0;
 }
